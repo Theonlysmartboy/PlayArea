ce55946357dd683828c73f988a8e61e1986837cc
Theonlysmartboy <o2jose43@gmail.com>
2026-02-05 14:00:27 +0300

    Minor update

--- Java/TwoDigitNumberSolver.java
+++ Java/TwoDigitNumberSolver.java
@@ -3,12 +3,10 @@ public class TwoDigitNumberSolver {
         for (int num = 10; num <= 99; num++) {
             int tens = num / 10;
             int units = num % 10;
-
             // Condition 1: sum of digits is 10
             if (tens + units != 10) {
                 continue;
             }
-
             // Condition 2: reversed number exceeds original by 18
             int reversed = units * 10 + tens;
             if (reversed - num == 18) {
--- Java/TwoDigitNumberSolverCLI.java
+++ Java/TwoDigitNumberSolverCLI.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class TwoDigitNumberSolverCLI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -12,7 +11,6 @@ public class TwoDigitNumberSolverCLI {
         for (int num = 10; num <= 99; num++) {
             int tens = num / 10;
             int units = num % 10;
-
             if ((tens + units == sumDigits) && ((units * 10 + tens) - num == diff)) {
                 System.out.println("Original number: " + num + ", Reversed number: " + (units * 10 + tens));
                 found = true;
